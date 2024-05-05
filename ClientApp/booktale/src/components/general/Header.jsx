5379dffe01dca6cba193b4734ba902f74ca9dd29
darkfos <darkfos82@gmail.com>
2024-05-05 23:16:48 +0300

    Updated header

--- ClientApp/booktale/src/components/general/Header.jsx
+++ ClientApp/booktale/src/components/general/Header.jsx
@@ -1,10 +1,14 @@
 import {useNavigate} from "react-router-dom";
+import "./Header.css";
 
 export default function Header() {
     const navigate_site = useNavigate();
 
     return (
         <header>
+            <div className="logo">
+                <a href="/">BookTale</a>
+            </div>
             <nav className="">
                 <a href="/">Главная</a>
                 <a href="/library">Библиотека</a>
@@ -12,7 +16,7 @@ export default function Header() {
                 <a href="/profile">Мой профиль</a>
                 <button onClick={() => {
                     navigate_site("/search");
-                }}>Поиск</button>
+                }} className="btn_search">Поиск</button>
             </nav>
         </header>
     )
