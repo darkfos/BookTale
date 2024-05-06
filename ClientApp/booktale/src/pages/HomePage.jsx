bda511c69fbd8da3d5bdd33d248b06e03d60a4c8
darkfos <darkfos82@gmail.com>
2024-05-06 13:53:34 +0300

    fix bg

--- ClientApp/booktale/src/pages/HomePage.jsx
+++ ClientApp/booktale/src/pages/HomePage.jsx
@@ -1,13 +1,25 @@
 import "./PagesCss.css";
+import "./HomeCss.css";
 import ButtonMain from "../components/buttons/ButtonMainPage";
+import img_main_1 from "../static/main_photo_1.png";
+import img_main_2 from "../static/main_photo_2.png";
 
 export default function Home() {
     return (
-            <main className="container-main">
-                <div className="header">
-                    <h1>Ваша любимая библиотека теперь в браузере!</h1>
-                    <ButtonMain text="Найти книгу"/>
-                </div>            
-            </main>
+        <main className="container-main">
+            <div className="header">
+                <h1>Ваша любимая библиотека теперь в браузере!</h1>
+                <ButtonMain text="Найти книгу"/>
+            </div>  
+            <div className="content-1">
+                <div className="content-1-left">
+                    <img src={img_main_1} alt="Негр читает книгу" />
+                </div>
+                <div className="content-1-right">
+                    <img src={img_main_2} alt="Женщина считает книги" />
+                </div>
+            </div>          
+            <div className="content-2"></div>
+        </main>
         )
 }
