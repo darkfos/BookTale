40a0f47e5ad19c69fcb844d51c6a7b70b9db1d28
darkfos <darkfos82@gmail.com>
2024-05-06 15:00:30 +0300

    Update btn for library page

---
+++ ClientApp/booktale/src/components/buttons/BrownButton.jsx
@@ -0,0 +1,7 @@
+import "../../pages/LibraryCss.css";
+
+export default function BrownButton(props) {
+    return (
+        <button className="btn-my-posts">{ props.text }</button>
+    )
+}
--- ClientApp/booktale/src/pages/LibraryPage.jsx
+++ ClientApp/booktale/src/pages/LibraryPage.jsx
@@ -1,9 +1,18 @@
 import "./PagesCss.css";
+import "./LibraryCss.css";
+import ButtonMain from "../components/buttons/ButtonMainPage";
+import BrownButton from "../components/buttons/BrownButton";
 
 export default function Library() {
     return (
         <main className="container-main">
-            <h1>Добро пожаловать на страницу Библиотека!</h1>
+            <div className="header">
+                <h1>Добро пожаловать в библиотеку, здесь вы можете опубликовать свой материал</h1>
+                <div className="btn-library">
+                   <ButtonMain text="Создать"/>
+                   <BrownButton text="Мои записи"/>
+                </div>
+            </div>
         </main>
     )
 }
