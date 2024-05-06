1f56a57d760c4db35ac8db341321c2f4de0bfe72
darkfos <darkfos82@gmail.com>
2024-05-06 15:12:28 +0300

    Added library page

--- ClientApp/booktale/src/pages/LibraryPage.jsx
+++ ClientApp/booktale/src/pages/LibraryPage.jsx
@@ -2,6 +2,7 @@ import "./PagesCss.css";
 import "./LibraryCss.css";
 import ButtonMain from "../components/buttons/ButtonMainPage";
 import BrownButton from "../components/buttons/BrownButton";
+import LibraryImage from "../static/library.png";
 
 export default function Library() {
     return (
@@ -12,6 +13,14 @@ export default function Library() {
                    <ButtonMain text="Создать"/>
                    <BrownButton text="Мои записи"/>
                 </div>
+            </div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <div className="library-main-container">
+                <img src={ LibraryImage } alt="Уный парень поясняет за книги" />
+                <p><span className="color-for-gr-text">Библиотеки</span> — гардеробы, из которых умелые люди могут извлекать  кое-что для украшения, многое — для любопытства и еще больше для  употребления. ( Дж.Дайер)</p>
             </div>
         </main>
     )
