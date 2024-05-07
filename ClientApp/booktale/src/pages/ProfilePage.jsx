6de55c09af9a67847ca0cd21b373091c693cf82e
darkfos <darkfos82@gmail.com>
2024-05-07 17:49:51 +0300

    Update backend endpoint profile-information

--- ClientApp/booktale/src/pages/ProfilePage.jsx
+++ ClientApp/booktale/src/pages/ProfilePage.jsx
@@ -6,8 +6,33 @@ import { Fragment } from "react";
 import Header from "../components/general/Header";
 import Footer from "../components/general/Footer";
 import BtnProfile from "../components/buttons/ButtonForProfile";
+import api from "../api";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import useAuthUser from "../hooks/use-auth";
+
+
 
 export default function Profile() {
+    const [profiledata, setProfileData] = useState(null);
+    const {login, token, refresh_token} = useAuthUser;
+
+    const getProfileData = async () => {
+        const response = await api.get("/user/profile-information", {
+            headers: {
+                Authorization: "Bearer " + token
+            }
+        });
+        console.log("dasdasd")
+        if (response.status == "200") {
+            setProfileData(response.data);
+        }
+    }
+
+    //Вызов функции
+    getProfileData();
+
     return (
         <Fragment>
             <Header />
@@ -15,7 +40,7 @@ export default function Profile() {
                 <div className="profile-info">
                     <div className="left-profile-info">
                         <img src="" alt="Фото профиля человека" />
-                        <p></p>
+                        <p>{profiledata.username}</p>
                         <div className="btn-profile">
                             <BtnProfile text="Изменить имя" />
                             <BtnProfile text="Изменить фото" />
@@ -26,8 +51,8 @@ export default function Profile() {
                     <div className="right-profile-info">
                         <h2>Статистика</h2>
                         <div className="body-right-info">
-                            <p>Количество публикаций</p>
-                            <p>Количество отзывов</p>
+                            <p>Количество публикаций {profiledata.count_books}</p>
+                            <p>Количество отзывов {profiledata.count_reviews}</p>
                         </div>
                         <div className="body-right-footer">
                             <h3>Мои книги</h3>
