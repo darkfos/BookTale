bec6b6c9d10194fd9732f89f11a3fa5827a23400
darkfos <darkfos82@gmail.com>
2024-05-09 00:35:24 +0300

    Added page for create a new book

--- ClientApp/booktale/src/pages/CreateBook.jsx
+++ ClientApp/booktale/src/pages/CreateBook.jsx
@@ -3,7 +3,9 @@ import api from "../api";
 import { Outlet } from "react-router-dom";
 import Header from "../components/general/Header";
 import Footer from "../components/general/Footer";
+import CreateBook from "../components/buttons/ButtonCreateBook";
 import "./PagesCss.css";
+import "./createBook.css";
 
 
 
@@ -12,7 +14,18 @@ export default function MyBooks() {
         <Fragment>
             <Header />
             <main className="container-main">
-                <h2>Создание книги</h2>
+                <header>
+                    <h1>Создание книги</h1>
+                </header>
+                <form action="" className="createBook">
+                    <label for="title">Ваш заголовок</label>
+                    <input type="text" placeholder="Заголовок" id="title"/>
+                    <label for="description">Описание вашей книги</label>
+                    <textarea name="" id="description" placeholder="Описание"></textarea>
+                    <div className="btn-form">
+                        <CreateBook text_1="Загрузить файл" text_2="Загрузить фотографию" page_1="/home" page_2="/home"/>
+                    </div>
+                </form>
             </main>
             <Footer />
             <Outlet />
